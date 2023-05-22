e4c4e9ff4049abc2b413b49a07ec1e7ab73ba8b1
Crhysss <crhystel.velasco@udla.edu.ec>
2023-05-22 16:15:54 -0500

    Ya el ultimo documentado, creo

--- leccio_pro.c
+++ leccio_pro.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* Las bibliotecas stdlib.h y time.h permiten que las funciones srand y rand cumplan su funcion*/
 /*Las variable "y" y x son los que guardan la informacion que ingresa el usuario sobre las dimensiones que quiere
 que sea su matriz*/
 int main(){
     int i = 0, j = 0, y, x;
     
-    printf("Ingrese las dimensiones de la matriz: \n");
+    printf("Ingrese las dimensiones de la matriz (en este orden:filas; columnas): \n");
     scanf("%d", &y);
     scanf("%d", &x) ;
-    /*Esta funcion srand es la encargada de generar los numeros random
+    /*Esta funcion srand es la encargada de generar o bueno indicar que se van a generar numeros random 
     y que se registren o bueno se guerden en la matriz[y][x]*/
     int matriz[y][x];
     srand(time(NULL));
-    
+    printf("Matriz original\n");
     for(i = 0; i < y; i++){
         for(j = 0; j < x; j++){
-            //esta parte se encarga de que establecer el rango de los numeros random//
-            matriz[i][j] = rand()%100;
+            /*esta parte se encarga de que establecer el rango de los numeros random
+            el rango es hasta 100*/
+            matriz[i][j] = rand() % 101;
             printf("%d ", matriz[i][j]);
         }
         
