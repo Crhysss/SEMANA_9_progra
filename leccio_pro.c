9f8f5de4b0bc6080a52319d64b20d2bcbf078820
Crhysss <crhystel.velasco@udla.edu.ec>
2023-05-22 15:53:17 -0500

    Intento de imprimir con numeros random

--- leccio_pro.c
+++ leccio_pro.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
     int i=0, j=0,y,x;
     int matriz[i][j];
+    srand(time(NULL));
     //"y" y x son las variable que guardan las dimensiones ingresadas por el usuario//
     printf("Ingrese las dimensiones de la matriz\n");
     scanf("%d", &y);
     scanf("%d", &x);
     for(i=0;i<y;i++){
         for(j=0;j<x;j++){
+            matriz[i][j]=rand() % 0+100;
             printf("%d ", matriz[y][x]);
         }
         printf("\n");
